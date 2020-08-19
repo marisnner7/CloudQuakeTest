53789335161042aebe3aeaa821344de33d3fa0c2
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-19 00:27:09 -0300

    created method initialize and start_game

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,22 @@
+require_relative 'game'
 class Parser
+  attr_accessor :log, :games
+
+  WORLD = "1022"
+
+  def initialize(log)
+    @log = log
+    @counter = 0
+    @games = []
+    
+  end
+
+  def start_game
+    @games[@counter += 1] = Game.new(@counter)
+    @map = Hash.new
+    @map[WORLD] = "<world>"
+    
+  end
+
   
 end
